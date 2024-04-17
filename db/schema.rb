3b9d0d99f61f2846e8aaa42f1b6e5f23008556fe
Andrew Vollhoffer <on.vollhoffer.a@gmail.com>
2024-04-17 14:16:53 -0400

    Fleshed out users table with names, email, bio

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_175256) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_181020) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.string "email"
   end
 
 end
