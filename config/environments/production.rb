f4b8247f103ab5def56c554dc31769a117a3f4c1
Andrew Vollhoffer <on.vollhoffer.a@gmail.com>
2024-06-23 20:25:38 -0400

    adjusted home page scrollspy div locations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "guardian_link_production"
+  # config.active_job.queue_name_prefix = "guardian_link_production" 
 
   config.action_mailer.delivery_method = :resend
   config.action_mailer.perform_caching = true
