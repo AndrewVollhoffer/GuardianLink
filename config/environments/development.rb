5fc82c253001aea2b03236bc5891bf180f996145
Andrew Vollhoffer <on.vollhoffer.a@gmail.com>
2024-07-31 10:21:13 -0400

    fixed email/name rendering in profile dropdown

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,13 +39,13 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :test
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
   #   arguments: %w[ -i ]
   # }
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = :false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: "localhost:3000"}
 
